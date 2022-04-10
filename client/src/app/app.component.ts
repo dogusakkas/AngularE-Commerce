46945a24b48b2682831beb067c894222cc805396
Dogus Akkas <dogusakkas@gmail.com>
2022-04-10 15:57:26 +0300

    component, product ve page model yapılarına bağlandı.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { IProduct } from './models/IProduct';
+import { IPagination } from './models/IPageination';
 
 @Component({
   selector: 'app-first',
@@ -8,12 +10,12 @@ import { HttpClient } from '@angular/common/http'
 })
 export class AppComponent implements OnInit{
   title = 'e-ticaret';
-  products :any[] = [];
+  products :IProduct[] = [];
 
   constructor(private http:HttpClient){}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:44396/api/Products').subscribe((response: any)=>{
+    this.http.get<IPagination>('https://localhost:44396/api/Products').subscribe((response: IPagination)=>{
 
       this.products=response.data;
 
