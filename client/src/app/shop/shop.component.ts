d4b493ecd2fa1282aea09598dcd7dc6c1a9ecd7c
Dogus Akkas <dogusakkas@gmail.com>
2022-04-15 16:46:22 +0300

    Kategoriler listelendi. Tür veya markaya göre listeleme altyapısı oluşturuldu.

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../shared/models/IProduct';
+import { IBrand } from '../shared/models/productBrand';
+import { IType } from '../shared/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -10,16 +12,55 @@ import { ShopService } from './shop.service';
 export class ShopComponent implements OnInit {
 
   products: IProduct[] | undefined;
+  brands: IBrand[] | undefined
+  types: IType[] | undefined
+  brandIdSelected : number | undefined;
+  typeIdSelected : number | undefined;
 
   constructor(private shopService : ShopService) { }
 
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe(response =>{
-      this.products = response.data
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+
+  getProducts(){
+    this.shopService.getProducts(this.brandIdSelected,this.typeIdSelected).subscribe(response =>{
+      this.products = response!.data
       console.log(this.products)
     }, err =>{
       console.log(err);
     });
+  }
+
+  getBrands(){
+    this.shopService.getBrands().subscribe(response => {
+      var firstItem = {id:0,name:'All'}
+      this.brands = [firstItem,...response];
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  getTypes(){
+    this.shopService.getTypes().subscribe(response => {
+      var firstItem = {id:0,name:'All'};
+      this.types= [firstItem,...response];
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  onBrandSelected(brandId:number){
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId:number){
+    this.typeIdSelected=typeId;
+    this.getProducts();
   }
 
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { IPagination } from '../shared/models/IPageination';
+import { IBrand } from '../shared/models/productBrand';
+import { IType } from '../shared/models/productType';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +14,32 @@ baseurl ="https://localhost:44396/api/";
 
   constructor(private http : HttpClient) { }
 
-getProducts(){
-  return this.http.get<IPagination>(this.baseurl + 'Products');
+getProducts(brandId? : number,typeId? : number){
+
+  let params = new HttpParams();
+
+  if(brandId){
+    params = params.append('brandId',brandId.toString());
+  }
+
+  if(typeId){
+    params = params.append('typeId',typeId.toString());
+  }
+
+  return this.http.get<IPagination>(this.baseurl + 'Products',{observe:'response',params})
+  .pipe(
+    map(response =>{
+      return response.body
+    })
+  );
+}
+
+getBrands(){
+  return this.http.get<IBrand[]>(this.baseurl + 'products/brands')
+}
+
+getTypes(){
+  return this.http.get<IType[]>(this.baseurl + 'products/types')
 }
 
 }
