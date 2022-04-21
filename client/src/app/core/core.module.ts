61db8cc728cad9b272875295e1a3754c18d6a378
Dogus Akkas <dogusakkas@gmail.com>
2022-04-22 00:29:57 +0300

    hata gösterim işlemi tamamlandı.

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,14 +4,20 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, NotFoundComponent, ServerErrorComponent],
+  declarations: [NavBarComponent, NotFoundComponent, ServerErrorComponent, TestErrorComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toastr-bottom-right',
+      preventDuplicates: true
+    })
   ],
   exports:[NavBarComponent]
 })
--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -1,21 +1,33 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
 import { catchError, Observable, throwError } from "rxjs";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
 
-constructor(private router: Router){}
+constructor(private router: Router, private toastr :ToastrService){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
             catchError(error =>{
                 if(error){
-                    if(error === 404){
+                    if(error.status === 400){
+                        if(error.error.errors){
+                            throw error.error;
+                        }
+                        else{
+                            this.toastr.error(error.error.message,error.error.statuscode);
+                        }
+                    }
+                    if(error.status === 401){
+                        this.toastr.error(error.error.message,error.error.statuscode);
+                    }
+                    if(error.status === 404){
                         this.router.navigateByUrl('/not-found');
                     }
-                    if(error === 500){
+                    if(error.status === 500){
                         this.router.navigateByUrl('/server-error');
                     }
                 }
